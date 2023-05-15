68657f4e52f9c73b1fd369cd9a19b7002cc8dd93
Elina <Elina@LAPTOP-L2JGPC2R>
2023-05-15 19:39:35 +0500

    Добавьте файлы проекта.

---
+++ oop6/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace oop6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button_circle = new System.Windows.Forms.Button();
+            this.button_square = new System.Windows.Forms.Button();
+            this.button_section = new System.Windows.Forms.Button();
+            this.button_triangle = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 196);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(267, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(218, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "ctrl";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(79, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(146, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 54);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "del";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button_circle
+            // 
+            this.button_circle.Location = new System.Drawing.Point(79, 10);
+            this.button_circle.Name = "button_circle";
+            this.button_circle.Size = new System.Drawing.Size(61, 54);
+            this.button_circle.TabIndex = 5;
+            this.button_circle.Text = "circle";
+            this.button_circle.UseVisualStyleBackColor = true;
+            this.button_circle.Click += new System.EventHandler(this.button_circle_Click);
+            // 
+            // button_square
+            // 
+            this.button_square.Location = new System.Drawing.Point(12, 10);
+            this.button_square.Name = "button_square";
+            this.button_square.Size = new System.Drawing.Size(61, 54);
+            this.button_square.TabIndex = 6;
+            this.button_square.Text = "square";
+            this.button_square.UseVisualStyleBackColor = true;
+            this.button_square.Click += new System.EventHandler(this.button_square_Click);
+            // 
+            // button_section
+            // 
+            this.button_section.Location = new System.Drawing.Point(218, 10);
+            this.button_section.Name = "button_section";
+            this.button_section.Size = new System.Drawing.Size(61, 54);
+            this.button_section.TabIndex = 7;
+            this.button_section.Text = "-----";
+            this.button_section.UseVisualStyleBackColor = true;
+            this.button_section.Click += new System.EventHandler(this.button_section_Click);
+            // 
+            // button_triangle
+            // 
+            this.button_triangle.Location = new System.Drawing.Point(151, 10);
+            this.button_triangle.Name = "button_triangle";
+            this.button_triangle.Size = new System.Drawing.Size(61, 54);
+            this.button_triangle.TabIndex = 8;
+            this.button_triangle.Text = "triangle";
+            this.button_triangle.UseVisualStyleBackColor = true;
+            this.button_triangle.Click += new System.EventHandler(this.button_triangle_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(60, 54);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "color";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(79, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 54);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "select";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.ClientSize = new System.Drawing.Size(776, 437);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button_triangle);
+            this.Controls.Add(this.button_section);
+            this.Controls.Add(this.button_square);
+            this.Controls.Add(this.button_circle);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trackBar1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "shapes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button_circle;
+        private System.Windows.Forms.Button button_square;
+        private System.Windows.Forms.Button button_section;
+        private System.Windows.Forms.Button button_triangle;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ oop6/Form1.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms.VisualStyles;
+using static oop6.Form1;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Collections.Specialized.BitVector32;
+
+namespace oop6
+{
+    public partial class Form1 : Form
+    {  
+        private List<CFigure> figures = new List<CFigure>(); // Лист для хранения всех фигур
+        public int objectSize = 10;
+        public bool Cntrl;
+
+        Color color = Color.Red;
+        Color red = Color.Red;
+        Color green = Color.Green;
+        Color purple = Color.Purple;
+        Color black = Color.Black;
+        int colorIndex = 0;
+
+        int selectedFigure = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                control.PreviewKeyDown += new PreviewKeyDownEventHandler(control_PreviewKeyDown);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)// Метод отрисовки кругов
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; // Сглаживание
+            foreach (CFigure figure in figures)
+            {
+                figure.SelfDraw(e.Graphics); // Метод круга для отрисовки самого себя
+            }
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!Cntrl)
+            {
+                foreach (CFigure figure in figures) // снятие выделения со всех объектов
+                {
+                    figure.setCondition(false);
+                }
+
+                switch (selectedFigure)
+                {
+                    case 0:
+                        CCircle newcircle = new CCircle(e.X, e.Y, objectSize, color);
+                        newcircle.setCondition(true);
+                        figures.Add(newcircle);
+                        break;
+                    case 1:
+                        CSquare newsquare = new CSquare(e.X, e.Y, objectSize, color);
+                        newsquare.setCondition(true);
+                        figures.Add((newsquare));
+                        break;
+                    case 2:
+                        CTriangle newtriangle = new CTriangle(e.X, e.Y, objectSize, color);
+                        newtriangle.setCondition(true);
+                        figures.Add((newtriangle));
+                        break;
+                    case 3:
+                        CSection newsection = new CSection(e.X, e.Y, objectSize, color);
+                        newsection.setCondition(true);
+                        figures.Add((newsection));
+                        break;
+                }
+                Refresh();
+            }
+            else if (Cntrl) // Выделение кругов, если зажат cntrl
+            {
+                foreach (CFigure figure in figures)
+                {
+                    if (figure.MouseCheck(e))
+                    {
+                        figure.setCondition(true);
+                        break;
+                    }
+                }
+                Refresh();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            objectSize = trackBar1.Value;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)// Отжатие кнопки
+        {
+            checkBox1.Checked = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)// Нажатие кнопок delete и cntrl
+        {
+            if (Control.ModifierKeys == Keys.Control)
+            {
+                checkBox1.Checked = true;
+            }
+            else if (e.KeyCode == Keys.Delete)
+            {
+                DelFigures();
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                foreach (CFigure figure in figures)
+                {
+                    if (figure.selected && ((figure.coords.Y - figure.rad) > 0))
+                    {
+                        figure.coords.Y -= 3;
+                    }
+                }
+                Refresh();
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                foreach (CFigure figure in figures)
+                {
+                    if (figure.selected && ((figure.coords.Y + figure.rad) < (int)this.ClientSize.Height))
+                    {
+                        figure.coords.Y += 3;
+                    }
+                }
+                Refresh();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                foreach (CFigure figure in figures)
+                {
+                    if (figure.selected && ((figure.coords.X - figure.rad) > 0))
+                    {
+                        figure.coords.X -= 3;
+                    }
+                }
+                Refresh();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                foreach (CFigure figure in figures)
+                {
+                    if (figure.selected && ((figure.coords.X + figure.rad) < (int)this.ClientSize.Width))
+                    {
+                        figure.coords.X += 3;
+                    }
+                }
+                Refresh();
+            }
+
+            else if (e.KeyCode == Keys.Oemplus)
+            {
+                GetBigger();
+            }
+            else if (e.KeyCode == Keys.OemMinus)
+            {
+                GetSmaller();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Cntrl = checkBox1.Checked;
+            foreach (CFigure figure in figures)
+            {
+                figure.fcntrl = Cntrl;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetBigger();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetSmaller();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DelFigures();
+        }
+        private void GetSmaller()
+        {
+            foreach (CFigure figure in figures)
+            {
+                if (figure.selected && figure.rad > 10)
+                {
+                    figure.rad -= 5;
+                }
+            }
+            Refresh();
+        }
+        private void GetBigger()
+        {
+            foreach (CFigure figure in figures)
+            {
+                if (figure.selected && figure.rad <= 95)
+                {
+                    figure.rad += 5;
+                }
+            }
+            Refresh();
+        }
+        void DelFigures() // Метод удаления фигур
+        {
+            for (int i = 0; i < figures.Count; i++)
+            {
+                if (figures[i].selected == true)
+                {
+                    figures.Remove(figures[i]);
+                    i--;
+                }
+            }
+            Refresh();
+        }
+
+        private void button_circle_Click(object sender, EventArgs e)
+        {
+            selectedFigure = 0;
+        }
+
+        private void button_square_Click(object sender, EventArgs e)
+        {
+            selectedFigure = 1;
+        }
+
+        private void button_triangle_Click(object sender, EventArgs e)
+        {
+            selectedFigure = 2;
+        }
+        private void button_section_Click(object sender, EventArgs e)
+        {
+            selectedFigure = 3;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (colorIndex < 3)
+                colorIndex++;
+            else
+                colorIndex = 0;
+            switch (colorIndex)
+            {
+                case 0:
+                    color = red;
+                    break;
+                case 1:
+                    color = green;
+                    break;
+                case 2:
+                    color = purple;
+                    break;
+                case 3:
+                    color = black;
+                    break;
+            }
+            button6.BackColor = color;
+            foreach (CFigure figure in figures)
+            {
+                if (figure.selected)
+                    figure.colorF = color;
+            }
+        }
+        void control_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                e.IsInputKey = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (CFigure figure in figures) // выделения всех объектов
+            {
+                figure.setCondition(true);
+            }
+            Refresh();
+        }
+        public class CFigure
+        {
+            public Point coords;
+            public int rad;
+            public bool selected = false;
+            public bool fcntrl = false;
+
+            public Color colorT = Color.CornflowerBlue;
+            public Color colorF = Color.Purple;
+
+            public void setCondition(bool cond) // метод переключения выделения
+            {
+                selected = cond;
+            }
+            public virtual void SelfDraw(Graphics g) // Метод для отрисовки самого себя
+            {
+
+            }
+            public virtual bool MouseCheck(MouseEventArgs e) // Проверка объекта на попадание в него курсора
+            {
+                return false;
+            }
+
+        }
+        public class CCircle : CFigure// класс круга
+        {
+            public CCircle(int x, int y, int radius, Color color) // конструктор по умолчанию
+            {
+                coords.X = x;
+                coords.Y = y;
+                rad = radius;
+                colorF = color;
+            }
+            public override void SelfDraw(Graphics g) // Метод для отрисовки самого себя
+            {
+                if (selected == true)
+                    g.DrawEllipse(new Pen(colorT, 3), coords.X - rad, coords.Y - rad, rad * 2, rad * 2);
+                else
+                    g.DrawEllipse(new Pen(colorF, 3), coords.X - rad, coords.Y - rad, rad * 2, rad * 2);
+            }
+            public override bool MouseCheck(MouseEventArgs e) // Проверка объекта на попадание в него курсора
+            {
+                if (fcntrl)
+                {
+                    if (Math.Pow(e.X - coords.X, 2) + Math.Pow(e.Y - coords.Y, 2) <= Math.Pow(rad, 2) && !selected)
+                    {
+                        selected = true;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        }
+
+        public class CSquare : CFigure // класс квадрата
+        {
+            public CSquare(int x, int y, int radius, Color color) // конструктор по умолчанию
+            {
+                coords.X = x;
+                coords.Y = y;
+                rad = radius;
+                colorF = color;
+            }
+            public override void SelfDraw(Graphics g) // Метод для отрисовки самого себя
+            {
+                if (selected == true)
+                    g.DrawRectangle(new Pen(colorT, 3), coords.X - rad, coords.Y - rad, rad * 2, rad * 2);
+                else
+                    g.DrawRectangle(new Pen(colorF, 3), coords.X - rad, coords.Y - rad, rad * 2, rad * 2);
+
+            }
+            public override bool MouseCheck(MouseEventArgs e) // Проверка объекта на попадание в него курсора
+            {
+                if (fcntrl)
+                {
+                    if (Math.Pow(e.X - coords.X, 2) + Math.Pow(e.Y - coords.Y, 2) <= Math.Pow(rad, 2) && !selected)
+                    {
+                        selected = true;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public class CTriangle : CFigure // класс треугольника
+        {
+            public CTriangle(int x, int y, int radius, Color color) // конструктор по умолчанию
+            {
+                coords.X = x;
+                coords.Y = y;
+                rad = radius;
+                colorF = color;
+            }
+            public override void SelfDraw(Graphics g) // Метод для отрисовки самого себя
+            {
+                Point point1 = new Point(coords.X, coords.Y - rad);
+                Point point2 = new Point(coords.X + rad, coords.Y + rad);
+                Point point3 = new Point(coords.X - rad, coords.Y + rad);
+                Point[] curvePoints = { point1, point2, point3 };
+
+                if (selected == true)
+                    g.DrawPolygon(new Pen(colorT, 3), curvePoints);
+                else
+                    g.DrawPolygon(new Pen(colorF, 3), curvePoints);
+            }
+            public override bool MouseCheck(MouseEventArgs e) // Проверка объекта на попадание в него курсора
+            {
+                if (fcntrl)
+                {
+                    if (Math.Pow(e.X - coords.X, 2) + Math.Pow(e.Y - coords.Y, 2) <= Math.Pow(rad, 2) && !selected)
+                    {
+                        selected = true;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public class CSection : CFigure // класс отрезка
+        {
+            public CSection(int x, int y, int radius, Color color) // конструктор по умолчанию
+            {
+                coords.X = x;
+                coords.Y = y;
+                rad = radius;
+                colorF = color;
+            }
+            public override void SelfDraw(Graphics g) // Метод для отрисовки самого себя
+            {
+                Point point1 = new Point(coords.X - rad, coords.Y);
+                Point point2 = new Point(coords.X + rad, coords.Y);
+                Point[] curvePoints = { point1, point2 };
+
+                if (selected == true)
+                    g.DrawPolygon(new Pen(colorT, 3), curvePoints);
+                else
+                    g.DrawPolygon(new Pen(colorF, 3), curvePoints);
+            }
+            public override bool MouseCheck(MouseEventArgs e) // Проверка объекта на попадание в него курсора
+            {
+                if (fcntrl)
+                {
+                    if (Math.Pow(e.X - coords.X, 2) + Math.Pow(e.Y - coords.Y, 2) <= Math.Pow(rad, 2) && !selected)
+                    {
+                        selected = true;
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+    }
+}
+
+
